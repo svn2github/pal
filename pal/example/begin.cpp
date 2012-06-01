0032a6c4aca107984175566d397438aeb8c4c7b2
aclong <aclong@62ce3681-6fdb-4e3c-a3e8-19904b470fb8>
2012-06-01 02:59:55 +0000

    minor example cleanup
    git-svn-id: https://svn.code.sf.net/p/pal/code@582 62ce3681-6fdb-4e3c-a3e8-19904b470fb8

--- pal/example/begin.cpp
+++ pal/example/begin.cpp
@@ -1,18 +1,20 @@
 #include <stdio.h> //for our old friend, the printf function
 #include "pal/palFactory.h"
 #include "pal/ConfigStatic.h"
+#include "pal/palCollision.h"
 
 const unsigned int NUM_ENGINES = 3;
 const char* ENGINES[NUM_ENGINES] = { "Bullet", "ODE", "Novodex" };
 
 int main(int argc, char *argv[]) {
 #ifndef PAL_STATIC
-	PF->LoadPhysicsEngines(); //use this if you wish to load PAL from .dll or .so
+    PF->LoadPhysicsEngines();
+    //use this if you wish to load PAL from .dll or .so
 	//otherwise, we can staticly link in an implementation by include the pal_i files
 	//eg: tokamak_pal.cpp and tokamak_pal.h
 #endif
 
-	for (int i = 0; i < NUM_ENGINES; i++) {
+	for (unsigned int i = 0; i < NUM_ENGINES; i++) {
 		if (PF->SelectEngine(ENGINES[i])) {
 			break;
 		}
@@ -28,10 +30,12 @@ int main(int argc, char *argv[]) {
 	pt->Init(0,0,0,50.0f); //initialize it, set its location to 0,0,0 and minimum size to 50
 	palBox *pb = PF->CreateBox(); //create a box
 	pb->Init(0,1.5f ,0, 1,1,1, 1); //initialize it, set its location to 0,1.5,0 (one and a half units up in the air), set dimensions to 1x1x1 and its mass to 1
+    palVector3 pos;
+    pb->GetPosition(pos); //get the location of the box
+    printf("Current box position is %6.5f at time %4.2f\n",pos.y,pp->GetTime());
 	for (int i=0;i<25;i++) { //run 25 steps of the simulation
 		pp->Update(0.02f); //update the physics engine. advance the simulation time by 0.02
 
-		palVector3 pos;
 		pb->GetPosition(pos); //get the location of the box
 
 		printf("Current box position is %6.5f at time %4.2f\n",pos.y,pp->GetTime());
